516cebffa56f432e45fad2cf3607216fe8cb4407
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-01-21 15:20:39 -0500

    Cleanup.

--- Nemesis-Mod/src/Hardware/Motor.cpp
+++ Nemesis-Mod/src/Hardware/Motor.cpp
@@ -14,14 +14,9 @@ void Motor::init(uint32_t directionPin, uint32_t pwmPin) {
 void Motor::start() {
     m_directionPin->write(HIGH);
     m_pwmPin->write(1);
-    // digitalWrite(m_directionPin, HIGH);
-    // analogWrite(m_pwmPin, 1);
 }
 
 void Motor::stop() {
     m_directionPin->write(LOW);
     m_pwmPin->write(0);
-    
-    // digitalWrite(m_directionPin, LOW);
-    // analogWrite(m_pwmPin, 0);
 }
