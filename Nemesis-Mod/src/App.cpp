a145f17cad9ef808e4d95357d8fae70583f5b1b3
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-01-23 21:19:50 -0500

    Moving the flywheel motor 2 to pin 10.

--- Nemesis-Mod/src/App.cpp
+++ Nemesis-Mod/src/App.cpp
@@ -6,8 +6,8 @@ App::App() {
 
 void App::init() {
     m_flywheelController = new FlywheelController();
-    m_flywheelController->init(FlywheelMotor::Motor1, 9); // L298->IN2
-    m_flywheelController->init(FlywheelMotor::Motor2, 7); // L298->IN4
+    m_flywheelController->init(FlywheelMotor::Motor1, 9);  // (OC1A) to L298->IN2
+    m_flywheelController->init(FlywheelMotor::Motor2, 10); // (OC1B) to L298->IN4
 
     m_revTrigger = new Button();
     m_revTrigger->init(12);
