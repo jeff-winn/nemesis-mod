0d3436bb29d6ba4c9d621c9690753b12c2e7c8b0
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-02-12 22:57:31 -0500

    Stubbing out functionality for firing rounds.

--- Nemesis-Mod/Nemesis-Mod.ino
+++ Nemesis-Mod/Nemesis-Mod.ino
@@ -7,12 +7,16 @@
 HAL* HardwareAccessLayer;
 FlywheelController* flywheelController;
 InterruptButton* revTrigger;
+InterruptButton* firingTrigger;
 
-// Identifies whether the hardware should continue execution.
+// Indicates whether the hardware should continue execution.
 volatile bool SHOULD_CONTINUE_EXECUTION = false;
 
-// Identifies whether the operator has authenticated prior to releasing the software lock.
+// Indicates whether the operator has authenticated prior to releasing the software lock.
 volatile bool HAS_OPERATOR_AUTHENTICATED = true;
+
+// Indicates whether the blaster should fire rounds at the target.
+volatile bool SHOULD_FIRE_ROUNDS = false;
 
 void setup() {
     set_sleep_mode(SLEEP_MODE_PWR_SAVE);
@@ -31,6 +35,10 @@ void setup() {
     revTrigger = new InterruptButton(
         new InterruptPin(3, INT1, HardwareAccessLayer));
     revTrigger->init(revTriggerStateChangedCallback);
+
+    firingTrigger = new InterruptButton(
+        new InterruptPin(2, INT0, HardwareAccessLayer));
+    firingTrigger->init(firingTriggerStateChangedCallback);
 }
 
 void loop() {
@@ -43,6 +51,13 @@ void loop() {
     flywheelController->start();
     
     while (SHOULD_CONTINUE_EXECUTION) {
+        if (SHOULD_FIRE_ROUNDS) {
+            // Start firing rounds.
+        }
+        else {
+            // Stop firing rounds.
+        }
+
         delay(10);
     }
 
@@ -71,4 +86,8 @@ void attemptToWakeTheDevice() {
 void revTriggerStateChangedCallback() {
     SHOULD_CONTINUE_EXECUTION = revTrigger->isPressed();
     attemptToWakeTheDevice();
+}
+
+void firingTriggerStateChangedCallback() {
+    SHOULD_FIRE_ROUNDS = firingTrigger->isPressed();
 }
