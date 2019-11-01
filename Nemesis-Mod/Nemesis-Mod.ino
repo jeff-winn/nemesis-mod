7fd1c06c8a69bddd0751347cb8c8864782a955bb
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-11-01 15:14:09 -0400

    Updating pin numbers for nRF52 board.

--- Nemesis-Mod/Nemesis-Mod.ino
+++ Nemesis-Mod/Nemesis-Mod.ino
@@ -2,10 +2,10 @@
 #include "src/App.h"
 
 // Defines the driver which controls the flywheel motors.
-DualG2HighPowerMotorShield18v18 flywheelDriver = DualG2HighPowerMotorShield18v18(9, -1, 5, -1, A0, 10, -1, 6, -1, A1);
+DualG2HighPowerMotorShield18v18 flywheelDriver = DualG2HighPowerMotorShield18v18(31, -1, 27, -1, A0, 11, -1, 30, -1, A1);
 
 // Defines the driver which controls the belt feed motor.
-G2HighPowerMotorShield18v17 beltDriver = G2HighPowerMotorShield18v17(17, -1, 11, -1, A2);
+G2HighPowerMotorShield18v17 beltDriver = G2HighPowerMotorShield18v17(17, -1, 7, -1, A2);
 
 // Defines the FRAM module for persistent data storage.
 Adafruit_FRAM_I2C fram;
@@ -22,11 +22,11 @@ void setup() {
         new FeedController(
             &mainboard, &beltDriver, &config),
         new Button(
-            new DigitalPin(13, &mainboard)),
+            new DigitalPin(16, &mainboard)),
         new Button(
-            new DigitalPin(12, &mainboard)),
+            new DigitalPin(15, &mainboard)),
         new Button(
-            new DigitalPin(18, &mainboard), true),
+            new DigitalPin(28, &mainboard), true),
         new BluetoothManager(),
         &config,
         &mainboard
