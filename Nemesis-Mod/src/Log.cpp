c3bb30c95d961b4400c29ce1036cd8be9814ad47
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-11-11 11:38:34 -0500

    Switching constant for logging.

--- Nemesis-Mod/src/Log.cpp
+++ Nemesis-Mod/src/Log.cpp
@@ -6,31 +6,31 @@ Logger Log = Logger();
 #define size_of_array(type) (char *)(&type+1)-(char*)(&type)
 
 void Logger::println(const float value, const int precision) {
-#ifndef __RELEASE__
+#ifdef __ENABLE_LOGGING__
     Serial.println(value, precision);
 #endif
 }
 
 void Logger::println(const int value) {
-#ifndef __RELEASE__
+#ifdef __ENABLE_LOGGING__
     Serial.println(value);
 #endif
 }
 
 void Logger::println(const uint8_t value) {
-#ifndef __RELEASE__
+#ifdef __ENABLE_LOGGING__
     Serial.println(value);
 #endif
 }
 
 void Logger::println(const char value[]) {
-#ifndef __RELEASE__
+#ifdef __ENABLE_LOGGING__
     Serial.println(value);
 #endif
 }
 
 void Logger::waitForUsbConnection() {
-#ifndef __RELEASE__
+#ifdef __ENABLE_LOGGING__
     Serial.begin(115200);
     
     while (!Serial) {
