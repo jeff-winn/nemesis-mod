7ff2625361345eab651596eb34ce9cf9949be5a3
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-11-29 09:38:39 -0500

    Updating constants.

--- shared/Constants.h
+++ shared/Constants.h
@@ -4,4 +4,7 @@
 // Defines the I2C address of the slave NRF52840 device.
 const uint8_t NRF52840_I2C_ADDR = 0x1A;
 
+const uint8_t NRF52_CID_START_ADVERTISING = 0x01;
+const uint8_t NRF52_CID_CLEAR_BONDS = 0x02;
+
 #endif /* CONSTANTS_H */
