f0208b0c7c637d695ef658fe033150409f15058c
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-11-30 16:43:01 -0500

    Cleanup.

--- sketches/Bluetooth/Bluetooth/src/BLEController.h
+++ sketches/Bluetooth/Bluetooth/src/BLEController.h
@@ -17,22 +17,11 @@ class BLEController {
 
         void clearBonds();
 
-        void onRemoteCommandReceived(uint8_t type, uint8_t subtype, uint8_t* data, uint8_t len);
-        void notifyFlywheelCurrentMilliamps(uint32_t m1, uint32_t m2, bool isActive);
-
-        void notifyBeltCurrentMilliamps(uint32_t m1, bool isActive);
-    
-    protected:
-        bool shouldSendNotification(uint32_t lastNotificationAtMillis, uint32_t interval);
-
     private:
         BlasterService m_blasterService;
         ConfigurationService m_configService;
 
         BLEDis m_discoveryService;
-
-        uint32_t m_lastFlywheelMilliampsNotifyAtMillis;
-        uint32_t m_lastBeltMilliampsNotifyAtMillis;
 };
 
 extern BLEController BLE;
